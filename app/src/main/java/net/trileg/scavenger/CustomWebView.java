a88cb5e038dc41c0c31b7241d6e0fa5620b725bf
Kensuke Kosaka <k@trileg.net>
2016-11-25 01:03:23 +0900

    WebViewスワイプで履歴遷移できるようにCustomWebViewを作成し設定

---
+++ app/src/main/java/net/trileg/scavenger/CustomWebView.java
@@ -0,0 +1,89 @@
+package net.trileg.scavenger;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+
+
+public final class CustomWebView extends WebView {
+    Context context;
+    GestureDetector gestureDetector;
+//    int gesture_stx, gesture_sty;
+
+    public CustomWebView(Context context) {
+        super(context);
+        this.context = context;
+        gestureDetector = new GestureDetector(context, onGestureListener);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return (gestureDetector.onTouchEvent(event) || super.onTouchEvent(event));
+    }
+
+    private final GestureDetector.SimpleOnGestureListener onGestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return super.onDoubleTap(e);
+        }
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            return super.onDoubleTapEvent(e);
+        }
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return super.onDown(e);
+        }
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,float velocityY) {
+            float deltax,deltay,velo;
+            deltax = Math.abs(e1.getRawX()-e2.getRawX());
+            deltay = Math.abs(e1.getRawY()-e2.getRawY());
+            velo = Math.abs(velocityX);
+
+            //pref_browser_gesturevelo is how fast finger moves.
+            //pref_browser_gesturevelo set to 350 as default in my app
+            if (deltax > 200 && deltay < 90 && velo > 350) {
+                if (e1.getRawX() > e2.getRawX()) {
+                    if (canGoForward()){
+                        //Gesture : move forward
+                        goForward();
+                    }
+                    else{
+                        //Gesture : no more forward history
+                    }
+                } else if(e1.getRawX() < e2.getRawX()){
+                    if (canGoBack()){
+                        //Gesture : go back
+                        goBack();
+                    }
+                    else {
+                        //Gesture : no more backward history
+                    }
+                }
+            }
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+        @Override
+        public void onLongPress(MotionEvent e) {
+            super.onLongPress(e);
+        }
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+        @Override
+        public void onShowPress(MotionEvent e) {
+            super.onShowPress(e);
+        }
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            return super.onSingleTapConfirmed(e);
+        }
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return super.onSingleTapUp(e);
+        }
+    };
+}
--- app/src/main/java/net/trileg/scavenger/MainActivity.java
+++ app/src/main/java/net/trileg/scavenger/MainActivity.java
@@ -2,8 +2,8 @@ package net.trileg.scavenger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView)findViewById(R.id.webview);
+        CustomWebView webView = new CustomWebView(this);
+
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.activity_main);
+        relativeLayout.addView(
+                webView,
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.MATCH_PARENT)
+        );
+
 
         webView.setWebViewClient(new WebViewClient());
 

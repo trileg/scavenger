a850d567b29e8430a767d7e2d4bd21c5fde809bf
Kensuke Kosaka <k@trileg.net>
2016-11-25 00:27:00 +0900

    WebView追加

--- app/src/main/java/net/trileg/scavenger/MainActivity.java
+++ app/src/main/java/net/trileg/scavenger/MainActivity.java
@@ -1,7 +1,9 @@
 package net.trileg.scavenger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.google.co.jp");
+
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 }

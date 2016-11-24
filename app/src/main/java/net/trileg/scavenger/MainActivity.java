797808c3e542e1a93ac3dbcc6ee4bae240eed4a2
Kensuke Kosaka <k@trileg.net>
2016-11-25 01:51:31 +0900

    WebViewのページ読み込みをオーバーライドする処理を追加

--- app/src/main/java/net/trileg/scavenger/MainActivity.java
+++ app/src/main/java/net/trileg/scavenger/MainActivity.java
@@ -1,18 +1,22 @@
 package net.trileg.scavenger;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean firstLoad = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomWebView webView = new CustomWebView(this);
+        final CustomWebView webView = new CustomWebView(this);
 
         RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.activity_main);
         relativeLayout.addView(
@@ -22,8 +26,22 @@ public class MainActivity extends AppCompatActivity {
                         RelativeLayout.LayoutParams.MATCH_PARENT)
         );
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                if (firstLoad) {
+                    // 初めてページが読み込まれたら，ここの処理を行う
+                    view.loadUrl("https://www.firefly.kutc.kansai-u.ac.jp/");
+                    firstLoad = false;
+                }
+            }
 
-        webView.setWebViewClient(new WebViewClient());
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // ページ読み込みが終わったら，ここの処理を行う
+                firstLoad = true;
+            }
+        });
 
         webView.loadUrl("https://www.google.co.jp");
 
